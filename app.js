a4b73cffc698a3360f636c8fac14bb67e4c87b84
Daniyal Mushtaq <daniyal.musht@gmail.com>
2023-04-21 00:49:28 +0500

    add getLang function and changes in getYear

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 (async function (){
     const response = await fetch("./data.json");
     const movies = await response.json();
-    const genre_select = document.getElementById("genre-list");
-    const year_select = document.getElementById("year-list");
+    const genre_id = document.getElementById("genre-list");
+    const year_id = document.getElementById("year-list");
+    const lang_id = document.getElementById("language-list");
+    const rating_ip = document.getElementsByClassName("rating");
 
     const genre_list = [ "Action", "Science Fiction", "Adventure", "Fantasy", "Thriller", "Animation", "Family", "Comedy", "Crime", "Drama"
                         , "Music", "Romance", "Western", "History", "War", "Horror"]
-    const year_list = []
+    let year_list = []
+    let lang_list = []
 
+
+    // function getYear(list)
+    // {
+    //     movies.forEach((movie) => {
+    //         const val = movie.release_date.split("-")[0];
+    //         if(!(list.includes(val)))
+    //         {
+    //             list.push(val);
+    //         }
+    //     });
+    //     list.sort((a,b) => {
+    //         return b-a;
+    //     })
+    // }
 
     function getYear()
     {
-        movies.forEach((movie) => {
-            const val = movie.release_date.split("-")[0];
-            if(!(year_list.includes(val)))
-            {
-                year_list.push(val);
-            }
-        });
-        year_list.sort((a,b) => {
-            return b-a;
-        })
+        year_list = movies.map(movie => movie.release_date.split("-")[0]);
+        year_list = year_list.filter((value,index,self) => self.indexOf(value) === index);
+        year_list = year_list.sort((a,b) => b-a);
     }
-    // console.log(year_list);
 
-    // console.log(genre_list);
+    function getLang()
+    {
+        lang_list = movies.map(movie => movie.original_language);
+        lang_list = lang_list.filter((value,index,self) => self.indexOf(value) === index);
+    }
 
-    function Displaylists(element_list,element_id)
+    function DisplayDropDowns(element_list,element_id)
     {
         element_list.innerHTML = "";
         let count = 1;
@@ -40,6 +53,8 @@
     }
 
     getYear();
-    Displaylists(genre_list,genre_select);
-    Displaylists(year_list,year_select);
+    getLang();
+    Displaylists(genre_list,genre_id);
+    Displaylists(year_list,year_id);
+    Displaylists(lang_list,lang_id);
 })();
