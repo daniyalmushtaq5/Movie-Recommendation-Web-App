af3b1d6f5f7795712f381757146377cef7c90b91
Daniyal Mushtaq <daniyal.musht@gmail.com>
2023-06-28 16:28:50 +0500

    add showMovies function

--- app.js
+++ app.js
@@ -6,6 +6,12 @@
     const lang_id = document.getElementById("language-list");
     const rating_id = document.getElementById("rating-list");
     const btn_id = document.getElementById("btn-submit");
+    // const result_movie_ul = document.getElementById("movie-list");
+    // const result_rank_ul = document.getElementById("rank-list");
+    // const result_year_ul = document.getElementById("year-list");
+    const movie_table = document.querySelector(".movie-table");
+    const movie_table_head = document.querySelector(".movie-table thead tr")
+    const movie_table_data = document.querySelector(".movie-table tbody")
 
     const genre_list = [ "Action", "Science Fiction", "Adventure", "Fantasy", "Thriller", "Animation", "Family", "Comedy", "Crime", "Drama"
                         , "Music", "Romance", "Western", "History", "War", "Horror"]
@@ -55,12 +61,128 @@
         });
     }
 
-
     function showMovies(movies_list)
     {
-        
+        // movie_table.innerHTML = "";
+        movie_table_head.innerHTML = "";
+        movie_table_data.innerHTML = "";
 
+        
+        let rank = document.createElement("th");
+        let movie = document.createElement("th");
+        let year = document.createElement("th");
+
+        rank.setAttribute("class",'rank-heading');
+        movie.setAttribute("class",'movie-heading');
+        year.setAttribute("class",'year-heading');
+
+        rank.innerHTML = `<b>RANK</b>`;
+        movie.innerHTML = `<b>MOVIE</b>`;
+        year.innerHTML = `<b>YEAR</b>`;
+
+        movie_table.setAttribute('style','border: 2px solid black;');
+        // movie_table_head.setAttribute('style','border: 2px solid black;');
+
+        rank.setAttribute('style', 'border-inline-end: 2px solid black; border-block-end: 2px solid black;');
+        movie.setAttribute('style', 'border-inline-end: 2px solid black; border-block-end: 2px solid black;');
+        // rank.setAttribute('style', 'border-block-end: 2px solid black;');
+        // movie.setAttribute('style', 'border-block-end: 2px solid black;');
+        year.setAttribute('style', 'border-block-end: 2px solid black;');
+        
+        
+        movie_table_head.appendChild(rank);
+        movie_table_head.appendChild(movie);
+        movie_table_head.appendChild(year);
+
+        count = 1;
+
+        movies_list.forEach((movie)=> {
+            let tr = document.createElement("tr");
+            let th_rank = document.createElement("th");
+            let th_movie = document.createElement("th");
+            let th_year = document.createElement("th");
+
+            th_rank.setAttribute("class",'rank-th');
+            th_movie.setAttribute("class",'movie-th');
+            th_year.setAttribute("class",'year-th');
+
+            th_rank.innerHTML = count;
+            th_movie.innerHTML = `<div class="title"><b>${movie.title}</b> </div>
+            <div class="attributes">
+                <ul id="att-list">
+                    <li class="certification"> ${movie.certification} </li> 
+                    <li class="genre"> ${movie.genres[0]} </li>
+                    <li class="time"> ${movie.runtime} </li>
+                </ul>
+            </div>`
+
+            th_year.innerHTML = movie.release_date.split("-")[0]
+
+
+            th_rank.setAttribute('style', 'border-inline-end: 2px solid black; border-block-end: 2px solid black;');
+            th_movie.setAttribute('style', 'border-inline-end: 2px solid black; border-block-end: 2px solid black;');
+            th_year.setAttribute('style', 'border-block-end: 2px solid black;');
+
+            tr.appendChild(th_rank);
+            tr.appendChild(th_movie);
+            tr.appendChild(th_year);
+
+            movie_table_data.appendChild(tr);
+            count++;
+
+        });
+        
     }
+
+
+    // function showMovies(movies_list)
+    // {
+    //     result_rank_ul.innerHTML = "";
+    //     result_movie_ul.innerHTML = "";
+    //     result_year_ul.innerHTML = "";
+
+    //     let rank = document.createElement("li");
+    //     let movie = document.createElement("li");
+    //     let year = document.createElement("li");
+
+    //     rank.setAttribute("class",'rank-heading');
+    //     movie.setAttribute("class",'movie-heading');
+    //     year.setAttribute("class",'year-heading');
+
+    //     rank.innerHTML = `<b>RANK</b>`;
+    //     movie.innerHTML = `<b>MOVIE</b>`;
+    //     year.innerHTML = `<b>YEAR</b>`;
+
+    //     result_rank_ul.appendChild(rank);
+    //     result_movie_ul.appendChild(movie);
+    //     result_year_ul.appendChild(year);
+
+    //     count = 1;
+
+    //     movies_list.forEach((movie)=> {
+            
+    //         let li_rank = document.createElement("li");
+    //         let li_movie = document.createElement("li");
+    //         let li_year = document.createElement("li");
+    //         li_rank.innerHTML = count;
+    //         li_movie.innerHTML = `<div class="title"><b>${movie.title}</b> </div>
+    //         <div class="attributes">
+    //             <ul id="att-list">
+    //                 <li class="certification"> ${movie.certification} </li> 
+    //                 <li class="genre"> ${movie.genres[0]} </li>
+    //                 <li class="time"> ${movie.runtime} </li>
+    //             </ul>
+    //         </div>`
+
+    //         li_year.innerHTML = movie.release_date.split("-")[0]
+    
+    //         result_rank_ul.appendChild(li_rank);
+    //         result_movie_ul.appendChild(li_movie);
+    //         result_year_ul.appendChild(li_year);
+    //         count++;
+    //     });
+        
+    // }
 
     function filterMovies(genre_val,year_val, lang_val, rating_val)
     {
@@ -80,28 +202,30 @@
             result = result.filter(movie => movie.original_language.includes(lang_val));
         }
 
-        if(lang_val)
+        if(rating_val)
         {
             result = result.filter(movie => movie.vote_average >= rating_val);
         }
 
-        btn_id.addEventListener("click", showMovies(result));
+        return result;
     }
 
     function Searchfun()
     {
-        let genre_val = null, year_val = null, lang_val = null, rating_val = null;
+        let genre_val = null, year_val = null, lang_val = null, rating_val = null, results = null;
         
         genre_id.addEventListener('change', event =>{
             genre_val = event.target.value;
             console.log(genre_val);
-            console.log(filterMovies(genre_val,year_val,lang_val,rating_val));
+            results = filterMovies(genre_val,year_val,lang_val,rating_val);
+            console.log(results);
         });
 
         year_id.addEventListener('change', event =>{
             year_val = event.target.value;
             console.log(year_val);
-            console.log(filterMovies(genre_val,year_val,lang_val,rating_val));
+            results = filterMovies(genre_val,year_val,lang_val,rating_val);
+            console.log(results);
 
         });
     
@@ -109,17 +233,24 @@
         lang_id.addEventListener('change', event => {
             lang_val = event.target.value;
             console.log(lang_val);
-            console.log(filterMovies(genre_val,year_val,lang_val,rating_val));
+            results = filterMovies(genre_val,year_val,lang_val,rating_val);
+            console.log(results);
         });
 
         rating_id.addEventListener('change', event => {
             rating_val = +(event.target.value);
             console.log(rating_val);
-            console.log(filterMovies(genre_val,year_val,lang_val,rating_val));
+            results = filterMovies(genre_val,year_val,lang_val,rating_val);
+            console.log(results);
+        });
+
+        btn_id.addEventListener("click", ()=>{
+            showMovies(results);
         });
     }
     
     Searchfun();
+
 
     getYear();
     getLang();
